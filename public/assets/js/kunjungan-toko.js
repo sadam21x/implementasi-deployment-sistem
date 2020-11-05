cacf2d1910391ce0ab6d73663445204c319e88bb
sadam21x <sadam21x@gmail.com>
2020-11-05 07:26:19 +0700

    revisi geolocation #4

--- public/assets/js/kunjungan-toko.js
+++ public/assets/js/kunjungan-toko.js
@@ -158,8 +158,6 @@ function hitungJarakSalesDanToko () {
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var d = R * c; // Distance in m
 
-    $('#result').append(d);
-
     return d;
 
 }
